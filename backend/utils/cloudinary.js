58a53b8cbb15b028404cb45b1db04d3c9c724653
Steffi <steffisharma022.com>
2024-10-06 19:29:51 +0530

    cors policy updated

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name:drpx5nley,
-    api_key:622826395228141,
-    api_secret:rI8LADIAS85WHC66trGmEBKBtQ8
+    cloud_name:"drpx5nley",
+    api_key:"622826395228141",
+    api_secret:"rI8LADIAS85WHC66trGmEBKBtQ8"
 });
 export default cloudinary;
